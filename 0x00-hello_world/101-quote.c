5da89373543675fd164d4fe636bb1b03ac28ccfc
jibribaye <jibril.suleman55@gmail.com>
2023-02-18 12:44:04 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include <unistd.h>
 /**
  * main - A c program that prints a line to a standard error
@@ -8,4 +8,4 @@ int main(void)
 {
 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
-}	
+}
