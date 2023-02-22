b0556d8ef0b8c2adb68412844570147cf69af452
jibribaye <jibril.suleman55@gmail.com>
2023-02-21 18:02:15 -0800

    hot fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <main.h>
 
 /**
  * main - Write a program that prints _putchar, followed by a new line.
@@ -9,7 +8,7 @@
 int main(void)
 {
 	_putchar('_');
-	_putcher('p');
+	_putchar('p');
 	_putchar('u');
 	_putchar('t');
 	_putchar('c');
